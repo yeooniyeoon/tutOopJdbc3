b897e5e9a4d05ea559686bb2947fa0861f007ba5
ycyeon <ickite@naver.com>
2023-06-01 10:12:51 +0900

    first commit

---
+++ src/opgame/service/MinusOperatorLevelOne.java
@@ -0,0 +1,29 @@
+package opgame.service;
+
+public class MinusOperatorLevelOne implements IOperator {
+
+    @Override
+    public void setA(int a) {
+
+    }
+
+    @Override
+    public void setB(int b) {
+
+    }
+
+    @Override
+    public void generateQuestion(int max) {
+
+    }
+
+    @Override
+    public String getQuestionMsg() {
+        return null;
+    }
+
+    @Override
+    public boolean isEqueals(int answer) {
+        return false;
+    }
+}
